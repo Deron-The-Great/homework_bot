8545bd870343a5e9daa0ec7ee7b74f7ae3297867
Сергей Шубин <deronlgg@yandex.ru>
2022-12-15 16:33:14 +0300

    1

--- homework.py
+++ homework.py
@@ -23,7 +23,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 600
+RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -170,7 +170,7 @@ def main():
             message = f'Сбой в работе программы: {error}'
             logger.error(message, exc_info=True)
             send_message(bot=bot, message=message)
-        time.sleep(RETRY_TIME)
+        time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
