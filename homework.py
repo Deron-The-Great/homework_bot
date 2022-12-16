1cce2f3710071df730d8efa74de4c1db22e404af
Сергей Шубин <deronlgg@yandex.ru>
2022-12-16 12:29:33 +0300

    Hope its final

--- homework.py
+++ homework.py
@@ -10,7 +10,7 @@ import telegram
 from dotenv import load_dotenv
 from telegram.error import TelegramError
 
-from . import exceptions
+from . import custom_exceptions as exceptions
 
 load_dotenv()
 
